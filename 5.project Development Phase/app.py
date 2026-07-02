1efdeb8cd6672fec5b99301bc33a3495b942b0a5
hemanth5267 <hemanth0000022@gmail.com>
2026-07-02 08:02:21 +0530

    Fix port binding for Render deployment

--- 5.project Development Phase/app.py
+++ 5.project Development Phase/app.py
@@ -27,4 +27,5 @@ def predict():
         prediction_text='Best crop for given conditions is {}'.format(output))
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
